9923ca51c0e645377b58b54829fb279998ab1343
Thanh Phạm <thanhpham@Thanhs-MacBook-Air.local>
2023-03-10 12:00:54 +0700

    Init example project

---
+++ apps/docs/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { MyCounterButton } from 'ui';
+</script>
+
+<h1>My Great Docs</h1>
+<MyCounterButton />
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ packages/ui/index.ts
@@ -0,0 +1,2 @@
+export { default as MyCounterButton } from './components/MyCounterButton.svelte';
+export { default as Button } from './components/Button.svelte';
