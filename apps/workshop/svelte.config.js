588bfb35653ce4755374e5c96f915a847944d3b7
Thanh Phạm <thanh.pham@discoveryloft.com>
2023-03-27 16:47:36 +0700

    refactor(camera): remove storybook
    Move all storybook to `apps/workshop`

--- apps/workshop/svelte.config.js
+++ apps/workshop/svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import sveltePreprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	preprocess: [sveltePreprocess()],
 
 	kit: {
 		adapter: adapter()
