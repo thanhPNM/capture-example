d1ce13b36afa5dbb20df15b519c9338baa2c4e4f
Thanh Phạm <thanh.pham@discoveryloft.com>
2023-03-29 20:18:47 +0700

    fix(vite.config): config port for server

--- apps/camera/vite.config.js
+++ apps/camera/vite.config.js
@@ -1,11 +1,19 @@
+import { defineConfig, loadEnv } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import svg from '@poppanator/sveltekit-svg'
 
 /** @type {import('vite').UserConfig} */
 
-const config = {
-  plugins: [sveltekit(), tsconfigPaths(), svg()]
-};
-
-export default config;
+export default defineConfig(({ mode}) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    plugins: [sveltekit(), tsconfigPaths(), svg()],
+    server: {
+      port: env.PORT
+    },
+    preview: {
+      port: env.PORT
+    }
+  }
+})
