8f13aef65517b708ca6d3db320a0e180473d44d0
Thanh Phạm <thanh.pham@discoveryloft.com>
2023-03-30 10:02:13 +0700

    chore(wasp): remove wasp file and related code

--- apps/home/src/routes/+layout.svelte
+++ apps/home/src/routes/+layout.svelte
@@ -1,40 +1,31 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import { isMobileBrowser } from 'shared-utils';
-  import { Alert } from 'ui';
-  import { SimpleSDK } from '@store/index';
+    import { isMobileBrowser } from 'shared-utils'
+    import { Alert } from 'ui'
 
-  import 'src/app.postcss';
-  import 'src/global.scss';
+    import 'src/app.postcss'
+    import 'src/global.scss'
 
-  let isMobile = isMobileBrowser();
-  console.log('🚀 ~ file: +layout.svelte:7 ~ isMobile:', isMobile);
-
-  onMount(() => {
-    if (typeof $SimpleSDK.add !== 'undefined') {
-      console.log();
-    }
-  });
+    let isMobile = isMobileBrowser()
 </script>
 
 <div class="wrapper">
-  {#if !isMobile}
-    <Alert message="You're currently not using mobile." type="warning" />
-  {/if}
+    {#if !isMobile}
+        <Alert message="You're currently not using mobile." type="warning" />
+    {/if}
 
-  <slot />
+    <slot />
 </div>
 
 <style lang="scss">
-  .wrapper {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    background-image: linear-gradient(
-      to bottom right,
-      var(--theme-primary),
-      var(--theme-secondary)
-    );
-    padding: 8px;
-  }
+    .wrapper {
+        height: 100vh;
+        display: flex;
+        flex-direction: column;
+        background-image: linear-gradient(
+            to bottom right,
+            var(--theme-primary),
+            var(--theme-secondary)
+        );
+        padding: 8px;
+    }
 </style>
